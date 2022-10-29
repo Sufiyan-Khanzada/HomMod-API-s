92a52ff36734ec4b13ce904fb47edefb16ea771e
Sufiyan-Khanzada <sufiyankhanzada1254@gmail.com>
2022-10-29 06:44:31 +0500

    HomeMod

---
+++ HomeMod/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::post('register', [PassportController::class, 'register']);
+Route::post('login', [PassportController::class, 'login']);
+
+// put all api protected routes here
+Route::middleware('auth:api')->group(function () {
+    Route::post('user-detail', [PassportController::class, 'userDetail']);
+    Route::post('logout', [PassportController::class, 'logout']);
+});
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
